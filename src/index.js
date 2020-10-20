077d54d2c9cff4a8a58d8ccaa2ea49e8c9e9d3a2
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-10-20 14:23:14 +0300

    feat: implement first video screencast

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import _ from 'lodash';
+
+function component() {
+    const elem = document.createElement('div');
+    elem.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return elem;
+}
+
+document.body.appendChild(component());
