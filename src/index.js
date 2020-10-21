c01ffed21be662362219ad1bf8544116f894ce37
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-10-21 16:39:30 +0300

    feat: implement second video screencast

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import _ from 'lodash';
+import './style.css';
+import imageWebpack from './assets/images/webpack.png';
 
 function component() {
     const elem = document.createElement('div');
+    const imageElem = new Image();
+    imageElem.src = imageWebpack;
     elem.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    elem.appendChild(imageElem);
+    elem.classList.add('hello');
     return elem;
 }
 
